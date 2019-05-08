716a75ee98f79ab6402d6faee39b8b6299b8abf7
Faisal Aqdas <faisalaqdas@gmail.com>
2019-05-08 15:06:15 +0500

    adding pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ require("dotenv").config({
 });
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mobile Accessories Shop in pakistan`,
+    description: `This ecommerce store is use to sell mobile accessories.`,
+    author: `@faisal`,
   },
   plugins: [
     'gatsby-plugin-netlify',
@@ -56,17 +56,21 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "Mobile Accessories",
+        short_name: "MA",
+        start_url: "/",
+        background_color: "#FF6709",
+        theme_color: "#FF6709",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
       },
+
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
   ],
 }
