89179e27f3244a134935cd1ef993e4794c95ebb7
Faisal Aqdas <faisalaqdas@gmail.com>
2019-05-12 14:27:29 +0500

    remove ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,15 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel="preconnect"
-      href="https://cdn.snipcart.com"
-      crossorigin="crossorigin"
-    />,
-  ]);
-};
+// You can delete this file if you're not using itr 
