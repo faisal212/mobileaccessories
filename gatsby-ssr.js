ac3beaf2dfce6b2802adc73400defb0a44d3dd73
Faisal Aqdas <faisalaqdas@gmail.com>
2019-05-12 15:21:52 +0500

    added prefetch and preconnect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,28 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using itr 
+// You can delete this file if you're not using it
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <link
+            rel="dns-prefetch"
+            key="dns-prefetch-snipcart-cdn"
+            href="https://cdn.snipcart.com"
+            crossorigin
+        />,
+        <link
+            rel="preconnect"
+            key="preconnect-snipcart-app"
+            href="https://app.snipcart.com"
+            crossorigin
+        />,
+        <link
+            rel="dns-prefetch"
+            key="dns-prefetch-images-cdn"
+            href="https://images.ctfassets.net"
+            crossorigin
+        />
+    ]);
+};
