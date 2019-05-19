55d03457a3508b597216307d6e675b972763b1ad
Faisal Aqdas <faisalaqdas@gmail.com>
2019-05-19 10:38:19 +0500

    remove all ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,28 +1,28 @@
 
 
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+// /**
+//  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/ssr-apis/
+//  */
 
-import React from "react";
+// import React from "react";
 
-export const onRenderBody = ({ setHeadComponents }) => {
-    setHeadComponents([
-        <link
-        rel="preconnect"
-        key="preconnect-snipcartapp"
-        href="https://app.snipcart.com"
+// export const onRenderBody = ({ setHeadComponents }) => {
+//     setHeadComponents([
+//         <link
+//         rel="preconnect"
+//         key="preconnect-snipcartapp"
+//         href="https://app.snipcart.com"
         
-    />,
-        <link
-            rel="preconnect"
-            key="preconnect-snipcart-cdn"
-            href="https://cdn.snipcart.com"
-            crossorigin='anonymous'
-        />
+//     />,
+//         <link
+//             rel="preconnect"
+//             key="preconnect-snipcart-cdn"
+//             href="https://cdn.snipcart.com"
+//             crossorigin='anonymous'
+//         />
      
-    ]);
-};
+//     ]);
+// };
 
